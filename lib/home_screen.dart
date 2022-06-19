b6d3d9759102712a0e5c688c9c2f5d56c00b3b8b
lcyper <lcyper@gmail.com>
2022-06-19 15:03:30 -0300

    add padding to all

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,57 +10,57 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const SelectDate(),
-            BlocBuilder<ContentCubit, ContentState>(
-              bloc: context.read<ContentCubit>()..getData(DateTime.now()),
-              builder: (context, state) {
-                if (state is ContentError) {
-                  return Text(
-                    state.message,
-                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                          color: Theme.of(context).errorColor,
-                        ),
-                  );
-                } else if (state is ContentData) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
+        child: Padding(
+          padding: const EdgeInsets.all(14),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SelectDate(),
+              BlocBuilder<ContentCubit, ContentState>(
+                bloc: context.read<ContentCubit>()..getData(DateTime.now()),
+                builder: (context, state) {
+                  if (state is ContentError) {
+                    return Text(
+                      state.message,
+                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            color: Theme.of(context).errorColor,
+                          ),
+                    );
+                  } else if (state is ContentData) {
+                    return Text(
                       'El estudio de hoy es: ${state.message}',
                       style: Theme.of(context).textTheme.headline4?.copyWith(
                             color: Theme.of(context).colorScheme.primary,
                           ),
                       textAlign: TextAlign.center,
-                    ),
-                  );
-                } else {
-                  // (state is ContentLoading)
-                  return const CircularProgressIndicator.adaptive();
-                }
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 18),
-              child: Column(
-                children: [
-                  Text(
-                    'Dedicado en memoria de anonimo',
-                    style: Theme.of(context).textTheme.headline6?.copyWith(
-                          color: Theme.of(context).hintColor,
-                        ),
-                  ),
-                  Text(
-                    'Desarrollado por lcyper',
-                    style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                          color: Theme.of(context).hintColor,
-                        ),
-                  ),
-                ],
+                    );
+                  } else {
+                    // (state is ContentLoading)
+                    return const CircularProgressIndicator.adaptive();
+                  }
+                },
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(bottom: 18),
+                child: Column(
+                  children: [
+                    Text(
+                      'Dedicado en memoria de anonimo',
+                      style: Theme.of(context).textTheme.headline6?.copyWith(
+                            color: Theme.of(context).hintColor,
+                          ),
+                    ),
+                    Text(
+                      'Desarrollado por lcyper',
+                      style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                            color: Theme.of(context).hintColor,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
